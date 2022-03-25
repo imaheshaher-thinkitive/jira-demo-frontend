86e55703f4c91c4f0be1f6f89a0e9ad13c7f633a
imaheshaher-thinkitive <mahesh.aher@thinkitive.com>
2022-03-25 12:36:23 +0530

    Sidebar component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoadmapComponent } from './roadmap/roadmap.component';
+import { BoardComponent } from './board/board.component';
+import { AddItemComponent } from './add-item/add-item.component';
+import { ProjectSettingComponent } from './project-setting/project-setting.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoadmapComponent,
+    BoardComponent,
+    AddItemComponent,
+    ProjectSettingComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
